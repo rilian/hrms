67ce8b5d6a55c3b9e7ba199168c3c66b938e7108
rilian <dmitriyis@gmail.com>
2015-12-05 22:05:52 -0500

    person views

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,34 @@
 class PeopleController < ApplicationController
   def index
+    @q = Person.ransack(params[:q])
+    @q.sorts = 'updated_at desc' if @q.sorts.empty?
+    @people = @q.result.limit(params[:limit]).offset(params[:offset])
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(person_params)
+      redirect_to people_path, success: 'Person Updated'
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def person_params
+    params.require(:person).permit(:name, :city, :phone, :skype, :linkedin, :facebook, :wants_relocate,
+      :primary_tech, :english)
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,23 @@
 class Person < ActiveRecord::Base
+  PRIMARY_TECHS = %w(
+    Ruby
+    Frontend
+    Java
+    QA/BA
+    iOS
+    Android
+    Salesforce
+    Devops
+  )
+  ENGLISHS = %w(
+    None
+    Beginner
+    Intermediate
+    Advanced
+    Native
+  )
 
+  has_many :notes
+
+  validates :name, presence: true
 end
