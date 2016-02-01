3482648594b27d7935651255522ea15713953dcd
rilian <dmitriyis@gmail.com>
2016-02-01 15:30:04 +0200

    some n+1 improvements

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,7 @@ class PeopleController < ApplicationController
     @q = Person.ransack(params[:q])
     @q.sorts = 'updated_at desc' if @q.sorts.empty?
     @people = @q.result(distinct: true).limit(params[:limit]).offset(params[:offset])
-      .includes(:notes, :attachments)
+      .includes(:notes, :attachments, :assessments, :action_points)
   end
 
   def show
