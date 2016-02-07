32d75abf916baa347cd0738857e47bd6fadc4e03
rilian <dmitriyis@gmail.com>
2016-02-08 00:12:25 +0200

    load only actions for current day

--- app/mailers/action_points_mailer.rb
+++ app/mailers/action_points_mailer.rb
@@ -2,9 +2,13 @@ class ActionPointsMailer < ActionMailer::Base
   add_template_helper ApplicationHelper
 
   def daily_activity(user_id)
-    @time = Time.zone.now
     @user = User.find(user_id)
-    @action_points = ActionPoint.where(is_completed: false) # TODO: load only one date
+    @time = Time.zone.now
+    @action_points = ActionPoint
+      .where(
+        is_completed: false,
+        perform_on: @time.midnight..(@time.midnight + 1.day)
+      )
 
     mail(
       subject: "[HRMS] Actions digest for #{@time.strftime('%e %b %Y')}",
