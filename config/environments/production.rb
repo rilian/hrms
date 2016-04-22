d6c90f063acb709c15db065cb47aeb155b23e165
rilian <dmitriyis@gmail.com>
2016-04-22 17:57:54 +0300

    add EMAIL_PROVIDER_DOMAIN_NAME

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,14 +79,14 @@ Rails.application.configure do
 
   # Mailer
   config.action_mailer.default_url_options = { host: ENV['DOMAIN_NAME'] }
-  config.action_mailer.default_options = { from: "no-reply@#{ENV['DOMAIN_NAME']}" }
+  config.action_mailer.default_options = { from: "no-reply@#{ENV['EMAIL_PROVIDER_DOMAIN_NAME']}" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['EMAIL_PROVIDER_ADDRESS'],
     user_name: ENV['EMAIL_PROVIDER_USERNAME'],
     password: ENV['EMAIL_PROVIDER_API_KEY'],
     port: 587,
-    domain: ENV['DOMAIN_NAME'],
+    domain: ENV['EMAIL_PROVIDER_DOMAIN_NAME'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
