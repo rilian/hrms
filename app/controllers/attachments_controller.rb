f2d3431adc27025ae79c61f13d3f069f78b64fd7
rilian <dmitriyis@gmail.com>
2016-04-23 15:05:38 +0300

    workaround for 0 bytes downloads

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,6 +1,4 @@
 class AttachmentsController < ApplicationController
-  load_and_authorize_resource
-
   def index
     @q = Attachment.accessible_by(current_ability).ransack(params[:q])
     @q.sorts = 'created_at desc' if @q.sorts.empty?
@@ -18,9 +16,11 @@ class AttachmentsController < ApplicationController
   end
 
   def show
+    @attachment = Attachment.find(params[:id])
   end
 
   def new
+    @attachment = Attachment.new
   end
 
   def create
@@ -35,9 +35,11 @@ class AttachmentsController < ApplicationController
   end
 
   def edit
+    @attachment = Attachment.accessible_by(current_ability).find(params[:id])
   end
 
   def update
+    @attachment = Attachment.accessible_by(current_ability).find(params[:id])
     if @attachment.update(attachment_params.merge!(updated_by: current_user))
       log_event(entity: @attachment, action: 'updated')
       redirect_to attachments_path, flash: { success: 'Attachment updated' }
