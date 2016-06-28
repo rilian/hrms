dcae3a00dc45c0d1fe5e5495cdef1da26b659e43
rilian <dmitriyis@gmail.com>
2016-06-28 12:52:23 +0300

    display Vacancy tags on Tags page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
   def index
     @tags = Person.not_deleted.accessible_by(current_ability).tag_counts_on(:tags)
+    @vacancy_tags = Vacancy.order(created_at: :desc).pluck(:tag)
   end
 end
