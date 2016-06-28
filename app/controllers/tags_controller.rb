7d2f1d1a5d3057b9747d1e5d2f87628fcfc9b471
rilian <dmitriyis@gmail.com>
2016-06-28 13:28:26 +0300

    sort tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,7 @@
 class TagsController < ApplicationController
   def index
     @tags = Person.not_deleted.accessible_by(current_ability).tag_counts_on(:tags)
+      .sort { |t1, t2| t2.taggings_count <=> t1.taggings_count }
     @vacancy_tags = Vacancy.order(created_at: :desc).pluck(:tag)
   end
 end
