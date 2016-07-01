02d83e6a9aae0d4b72d3d605272e0d1e2b26939a
rilian <dmitriyis@gmail.com>
2016-07-01 15:53:42 +0300

    dayoff validations

--- app/models/dayoff.rb
+++ app/models/dayoff.rb
@@ -6,7 +6,7 @@ class Dayoff < ActiveRecord::Base
   belongs_to :person
   belongs_to :updated_by, class_name: 'User'
 
-  validates :person, presence: true
+  validates :person, :start_on, :end_on, presence: true
   validates :type, inclusion: { in: TYPES }
   validates :days, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 50 }
 end
