cd21da94c99e3fdab9f1f2ce1b89f79be06b4697
rilian <dmitriyis@gmail.com>
2016-07-08 12:57:17 +0300

    cleanup event params

---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,14 @@
+module EventsHelper
+  def cleanup_event_params(params)
+    params
+      .reject {|k,_| k.in? %w[created_at updated_at encrypted_password reset_password_token] }
+      .to_s
+      .gsub('=>[nil, ', '=>[')
+      .chomp('}')
+      .reverse
+      .chomp('{')
+      .reverse
+      .gsub('["", ', '[')
+      .gsub('\r\n', ' '.html_safe)
+  end
+end
