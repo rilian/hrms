b93aad965b51b46df71b7542d32ea2b8948df784
rilian <dmitriyis@gmail.com>
2016-07-13 15:57:48 +0300

    validate dayoff start_on for the same person

--- app/models/dayoff.rb
+++ app/models/dayoff.rb
@@ -9,4 +9,15 @@ class Dayoff < ActiveRecord::Base
   validates :person, :start_on, :end_on, presence: true
   validates :type, inclusion: { in: TYPES }
   validates :days, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 50 }
+
+  validate :dates_intersection
+
+  private
+
+  def dates_intersection
+    scope = Dayoff.where(person_id: person_id, start_on: start_on)
+    scope = scope.where('id != ?', self.id) if self.id.present?
+    return unless scope.exists?
+    errors.add(:start_on, 'already recorded for this person')
+  end
 end
