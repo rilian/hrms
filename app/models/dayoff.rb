96d6732e268656bb00dd2b1f4c2e8294eebd16a8
rilian <dmitriyis@gmail.com>
2016-08-01 13:57:37 +0300

    add more dayoff types

--- app/models/dayoff.rb
+++ app/models/dayoff.rb
@@ -1,7 +1,7 @@
 class Dayoff < ActiveRecord::Base
   self.inheritance_column = nil
 
-  TYPES = ['Vacation', 'Sick Leave']
+  TYPES = ['Vacation', 'Sick Leave', 'Unpaid Day Off', 'Paid Day Off']
 
   belongs_to :person, touch: true
   belongs_to :updated_by, class_name: 'User'
