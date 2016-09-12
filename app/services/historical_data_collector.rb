6770b65efb1a67cd47446296d2aaaa809772c73a
rilian <dmitriyis@gmail.com>
2016-09-12 17:19:25 +0300

    reorganize historical data loading

--- app/services/historical_data_collector.rb
+++ app/services/historical_data_collector.rb
@@ -1,27 +1,47 @@
 class HistoricalDataCollector
+  KLASSES = [ActionPoint, Attachment, Dayoff, Event, Note, Person, User, Vacancy]
+
   def initialize
     @data = {}
+
+    set_oldest_date
+    set_today
   end
 
   def perform
-    [ActionPoint, Attachment, Dayoff, Event, Note, Person, User, Vacancy].each do |klass|
+    KLASSES.each do |klass|
       table = klass.table_name
-      @data[table] = {}
-      date_today = Time.zone.now.strftime('%Y-%m-%d')
-      date_oldest = klass.first.created_at.strftime('%Y-%m-%d')
 
       query = "SELECT
         count(#{table}.id) AS cnt,
         to_char(d.day, 'YYYY-MM-DD') AS day
       FROM
-        generate_series('#{date_oldest}'::date, '#{date_today}'::date, '1d') AS d(day)
+        generate_series('#{@oldest_date}'::date, '#{@today}'::date, '1d') AS d(day)
         LEFT OUTER JOIN #{table} ON #{table}.created_at >= d.day
           AND #{table}.created_at < d.day + '1d'::interval
       GROUP BY d.day
       ORDER BY d.day"
 
-      @data[table] = ActiveRecord::Base.connection.execute(query).to_a
+      ActiveRecord::Base.connection.execute(query).to_a.each do |item|
+        @data[item['day']] = {} if @data[item['day']].nil?
+        @data[item['day']][table] = item['cnt']
+      end
     end
+
     @data
+  end
+
+  private
+
+  def set_oldest_date
+    date = Time.zone.now
+    KLASSES.each do |klass|
+      date = klass.first.created_at if date > klass.first.created_at
+    end
+    @oldest_date = date.strftime('%Y-%m-%d')
+  end
+
+  def set_today
+    @today = Time.zone.now.strftime('%Y-%m-%d')
   end
 end
