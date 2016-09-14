8251e7ed42293e1da10d1c19e37b2e200b690c14
rilian <dmitriyis@gmail.com>
2016-09-14 15:01:35 +0300

    extend similar ppl report by checking email, phone, skype

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,8 +10,15 @@ class ReportsController < ApplicationController
     previous_ids = []
     Person.not_deleted.accessible_by(current_ability).find_each(batch_size: 50) do |person|
       similars = Person.not_deleted.accessible_by(current_ability)
-                  .where('id NOT IN (?)', [person.id] + previous_ids)
-                  .where('lower(name) ILIKE ?', "%#{person.name.downcase}%")
+        .where('id NOT IN (?)', [person.id] + previous_ids)
+        .where("(lower(name) ILIKE ?)
+          OR (email IS NOT NULL AND email != '' AND email ILIKE ?)
+          OR (phone IS NOT NULL AND phone != '' AND phone ILIKE ?)
+          OR (skype IS NOT NULL AND skype != '' AND skype ILIKE ?)",
+          "%#{person.name.downcase.strip}%",
+          "%#{person.email.strip.presence || '#invalid#'}%",
+          "%#{person.phone.strip.presence || '#invalid#'}%",
+          "%#{person.skype.strip.presence || '#invalid#'}%")
 
       if similars.exists?
         @people << {
