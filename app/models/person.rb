0554030636087cddcf01531504d763a5b478cb18
rilian <dmitriyis@gmail.com>
2016-09-15 13:30:25 +0300

    validate uniqueness of linkedin

--- app/models/person.rb
+++ app/models/person.rb
@@ -39,7 +39,7 @@ class Person < ActiveRecord::Base
   validates :source, inclusion: { in: SOURCES }, allow_blank: true
   validates :primary_tech, inclusion: { in: PRIMARY_TECHS }
   validates :vacation_override, numericality: { only_integer: true}, allow_blank: true
-  validates :email, :phone, :skype, uniqueness: { case_sensitive: false }, allow_blank: true
+  validates :email, :phone, :skype, :linkedin, uniqueness: { case_sensitive: false }, allow_blank: true
 
   scope :not_deleted, ->() { where(is_deleted: false) }
   scope :employee, ->() { where(status: EMPLOYEE_STATUSES).order(:status) }
