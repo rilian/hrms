7408e62db8f6258ac78e0269c30025b21350e8ae
rilian <dmitriyis@gmail.com>
2017-07-11 16:43:21 +0300

    cleanup

--- app/helpers/dayoffs_helper.rb
+++ app/helpers/dayoffs_helper.rb
@@ -1,10 +1,4 @@
 module DayoffsHelper
-  def allowed_vacation(person)
-    return 0 unless person.start_date
-    vacation_per_year = person.vacation_override.to_i > 0 ? person.vacation_override : ENV['VACATION_PER_YEAR'].to_i
-    ((((person.finish_date || Date.today) - person.start_date).to_i.abs) / 365.25).ceil * vacation_per_year
-  end
-
   def vacation_stats_per_year(person)
     return {} unless person.start_date
 
@@ -49,32 +43,7 @@ module DayoffsHelper
       current_date += 1.year
     end
 
-    puts stats
     stats
-  end
-
-  def used_vacation(person)
-    person.dayoffs.where(type: 'Vacation').sum(:days)
-  end
-
-  def overtime_days(person)
-    person.dayoffs.where(type: 'Overtime').sum(:days)
-  end
-
-  def sick_leaves(person)
-    person.dayoffs.where(type: 'Sick Leave').sum(:days)
-  end
-
-  def unpaid_days_off(person)
-    person.dayoffs.where(type: 'Unpaid Day Off').sum(:days)
-  end
-
-  def paid_days_off(person)
-    person.dayoffs.where(type: 'Paid Day Off').sum(:days)
-  end
-
-  def working_days_shifts(person)
-    person.dayoffs.where(type: 'Working Day Shift').sum(:days)
   end
 
   def months_worked(person)
@@ -83,14 +52,44 @@ module DayoffsHelper
       (person.finish_date || Date.today).month - person.start_date.month
   end
 
-  def remaining_vacation(person)
-    (allowed_vacation(person) + overtime_days(person) - used_vacation(person)).ceil
-  end
-
-  def end_of_current_year(person)
-    person.start_date +
-      (Time.zone.now.year - person.start_date.year + (Time.zone.now.yday < person.start_date.yday ? 0 : 1)).years
-  end
+  # def allowed_vacation(person)
+  #   return 0 unless person.start_date
+  #   vacation_per_year = person.vacation_override.to_i > 0 ? person.vacation_override : ENV['VACATION_PER_YEAR'].to_i
+  #   ((((person.finish_date || Date.today) - person.start_date).to_i.abs) / 365.25).ceil * vacation_per_year
+  # end
+  #
+  # def used_vacation(person)
+  #   person.dayoffs.where(type: 'Vacation').sum(:days)
+  # end
+  #
+  # def overtime_days(person)
+  #   person.dayoffs.where(type: 'Overtime').sum(:days)
+  # end
+  #
+  # def sick_leaves(person)
+  #   person.dayoffs.where(type: 'Sick Leave').sum(:days)
+  # end
+  #
+  # def unpaid_days_off(person)
+  #   person.dayoffs.where(type: 'Unpaid Day Off').sum(:days)
+  # end
+  #
+  # def paid_days_off(person)
+  #   person.dayoffs.where(type: 'Paid Day Off').sum(:days)
+  # end
+  #
+  # def working_days_shifts(person)
+  #   person.dayoffs.where(type: 'Working Day Shift').sum(:days)
+  # end
+  #
+  # def remaining_vacation(person)
+  #   (allowed_vacation(person) + overtime_days(person) - used_vacation(person)).ceil
+  # end
+  #
+  # def end_of_current_year(person)
+  #   person.start_date +
+  #     (Time.zone.now.year - person.start_date.year + (Time.zone.now.yday < person.start_date.yday ? 0 : 1)).years
+  # end
 
   private
 
