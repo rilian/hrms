3f88a7c57a1aec09078a049f6ba247eeb2466380
rilian <dmitriyis@gmail.com>
2017-08-21 18:29:41 +0300

    allow _ in email

--- app/models/person.rb
+++ app/models/person.rb
@@ -48,7 +48,7 @@ class Person < ActiveRecord::Base
   validates :email, :phone, :skype, :linkedin, uniqueness: { case_sensitive: false }, allow_blank: true
   validates :name, format: { with: /\A[a-zA-Z\d\s\-\'\(\)]+\z/, message: 'invalid symbols. Only A-Z, digits, braces, quote and space allowed' }, allow_blank: true
   validates :phone, format: { with: /\A[\s\+\d]+\z/ }, allow_blank: true
-  validates :email, format: { with: /\A[0-9a-z\@\.\-\'\+]+\z/ }, allow_blank: true
+  validates :email, format: { with: /\A[0-9a-z\@\.\_\-\'\+]+\z/ }, allow_blank: true
   validates :start_date, presence: { message: 'should be present for current employee' }, if: ->(p) { p.status.in?(EMPLOYEE_STATUSES) }
   validates :email, :phone, presence: { message: 'should be present for current employee' }, if: ->(p) { p.status.in?(EMPLOYEE_STATUSES) && p.start_date.present? && p.start_date <= Time.zone.now }
 
