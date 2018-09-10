b102c2f5f21bf20ad2b17400d22cb7e47674cf65
rilian <dmitriyis@gmail.com>
2018-09-10 15:19:31 +0300

    fix migration

--- db/migrate/20180910115613_copy_email_to_personal_email_to_person.rb
+++ db/migrate/20180910115613_copy_email_to_personal_email_to_person.rb
@@ -1,7 +1,7 @@
 class CopyEmailToPersonalEmailToPerson < ActiveRecord::Migration[5.2]
   def change
-    Person.not_deleted.find_each(batch_size: 100) do |person|
-      person.update(personal_email: person.email)
-    end
+    ActiveRecord::Base.connection.execute('
+      UPDATE people SET personal_email = email
+    ')
   end
 end
