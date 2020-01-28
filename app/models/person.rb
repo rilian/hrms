e9d3e3f2d65776d76e877a88d399031c28978e11
rilian <dmitriyis@gmail.com>
2020-01-28 09:09:08 -0500

    allow `@` in skype

--- app/models/person.rb
+++ app/models/person.rb
@@ -66,7 +66,7 @@ class Person < ActiveRecord::Base
   validates :name, format: { with: /\A[a-zA-Z\d\s\-\'\(\)]+\z/, message: 'invalid symbols. Only A-Z, digits, braces, quote and space allowed' }, allow_blank: true
   validates :phone, format: { with: /\A[\s\+\,\d]+\z/ }, allow_blank: true
   validates :email, :personal_email, format: { with: /\A[0-9a-z\@\.\_\-\'\+]+\z/ }, allow_blank: true
-  validates :skype, format: { with: /\A[0-9a-z\.\:\_\-]+\z/ }, allow_blank: true
+  validates :skype, format: { with: /\A[0-9a-z\.\:\_\-\@]+\z/ }, allow_blank: true
   validates :start_date, presence: { message: 'should be present for current employee' }, if: ->(p) { p.status.in?(EMPLOYEE_STATUSES) && !p.new_record? }
   validates :email, presence: { message: 'should be present for current employee' }, if: ->(p) { p.status.in?(CURRENT_EMPLOYEE_STATUSES) && p.start_date.present? && p.start_date <= Time.zone.now }
   validate :finish_date_greater_or_equal_start_date
